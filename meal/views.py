b1e3e2c3e146639028ddf538f724ff7a5b115505
Suresh <suresh.p@coolwinks.com>
2020-06-01 00:56:00 +0530

    Done

--- meal/views.py
+++ meal/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from meal.models import get_cuisines_foods, get_good_food_for_medical, get_bed_food_for_medical, get_good_food_for_allergy, get_bed_food_for_allergy, \
-    Meal
+    Meal, get_user_like_food, get_user_dislike_food
 
 
 def get_meal(user_id, cuisines, medical, allergies):
@@ -24,8 +24,11 @@ def get_meal(user_id, cuisines, medical, allergies):
         allergies_good += get_good_food_for_allergy(a)
         allergies_bed += get_bed_food_for_allergy(a)
 
-    final_food_like = final_food_like.union(allergies_good).union(cuisines_food).union(medical_good)
-    final_list = final_food_like.difference(medical_bed).difference(allergies_bed)
+    user_like_food = get_user_like_food(user_id=user_id)
+    user_dislike_food = get_user_dislike_food(user_id=user_id)
+
+    final_food_like = final_food_like.union(allergies_good).union(cuisines_food).union(medical_good).union(user_like_food)
+    final_list = final_food_like.difference(medical_bed).difference(allergies_bed).difference(user_dislike_food)
     meals = Meal.objects.filter(foods__id__in=final_list).annotate(total=Count('id')).order_by('-total')
 
     return meals[:10]
